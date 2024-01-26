4952b3354b1a89fc4e2c19dbb56b298e2c9b4624
Pixol20 <115364463+pixol20@users.noreply.github.com>
2024-01-26 07:25:41 +0200

    added typewritter text

--- src/static/main.js
+++ src/static/main.js
@@ -4,7 +4,36 @@ $(document).ready(function()
     {
         $.ajax({url: "/generate", type:"POST", success: function(response)
         {
-            $("#textPlaceholder").html(response);
+            placeholder = $("#textPlaceholder")
+            var speed = 30;
+            var letterIndex = 0;
+            // Function to erase text letter by letter and write a new one
+            function eraseAndWrite() 
+            {
+                var textContent = placeholder.text();
+                var textLength = textContent.length;
+            
+                var interval = setInterval(function() {
+                        textLength--;
+                        placeholder.text(textContent.substr(0, textLength));
+                
+                        if (textLength === 0) {
+                            clearInterval(interval);
+                            writeText();
+                        }
+                    }, speed);
+            }
+
+            function writeText()
+            {
+                if(letterIndex < response.length)
+                {
+                    placeholder.html(placeholder.html()+response[letterIndex]);
+                    letterIndex++;
+                    setTimeout(writeText, speed);
+                }
+            }
+            eraseAndWrite();
         }});
     });
 });

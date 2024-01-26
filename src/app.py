38112c620df56195fb03bad6cc4b3fa672b6c563
Pixol20 <115364463+pixol20@users.noreply.github.com>
2024-01-26 06:27:46 +0200

    added limiter

--- src/app.py
+++ src/app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import model
 app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["2 per minute"],
+    storage_uri="memory://",
+)
 model.LoadModel()
 @app.route("/")
+@limiter.exempt()
 def index():
     return render_template("index.html")
 
